a33598c0f35e36c15bf6a671353220efcf738da5
Yaxing Cai <caiyaxing666@gmail.com>
2023-04-14 20:49:18 +0000

    upd

--- python.sh
+++ python.sh
@@ -1,6 +1,6 @@
 sudo apt install build-essential ccache
 # https://apt.llvm.org/
-sudo apt install clang-15 lldb-15 lld-15
+sudo apt install clang lldb lld clang-format clang-tidy
 # https://docs.conda.io/en/latest/miniconda.html
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
@@ -8,4 +8,4 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 conda create -n python310 python=3.10
 pip3 install cmake
 conda install -c anaconda numpy scipy decorator pytest
-conda install -c conda-forge psutil typing-extensions
+conda install -c conda-forge psutil typing-extensions black
