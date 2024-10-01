5a58675956161682c6dcdabf9d9d865c676ed3b8
Yaxing Cai <caiyaxing666@gmail.com>
2024-10-01 22:17:15 +0000

    upd

--- compile.sh
+++ compile.sh
@@ -1,16 +1,58 @@
-if [ "$#" -eq 0 ] ; 
-then
-    echo 'compiling tvm'
-    cd tvm
-else
-    echo 'compiling '$1
-    cd $1
+TVM=0
+MLC_LLM=0
+
+for (( i=0; i<${#1}; i++ )); do
+    option="${1:$i:1}"
+    if [ $option = "t" ]; then
+        TVM=1
+    elif [ $option = "m" ]; then
+        MLC_LLM=1
+    fi
+done
+
+current_dir=$PWD
+
+if [ $TVM -eq 1 ]; then
+    echo "build tvm at $current_dir/tvm/"
+    mkdir $current_dir/tvm/build -p
+    cd $current_dir/tvm/build
+    cp ../cmake/config.cmake .
+    echo "set(USE_LLVM ON)" >> config.cmake
+    echo "set(USE_CUDA ON)" >> config.cmake
+    echo "set(USE_NVTX ON)" >> config.cmake
+    echo "set(USE_GRAPH_EXECUTOR ON)" >> config.cmake
+    echo "set(USE_GRAPH_EXECUTOR_CUDA_GRAPH ON)" >> config.cmake
+    echo "set(USE_VLLM ON)" >> config.cmake
+    echo "set(USE_THRUST ON)" >> config.cmake
+    echo "set(USE_CUTLASS ON)" >> config.cmake
+    echo "set(USE_CUBLAS ON)" >> config.cmake
+    echo "set(USE_NCCL ON)" >> config.cmake
+    echo "set(USE_FLASHINFER ON)" >> config.cmake
+    echo "set(CMAKE_CUDA_ARCHITECTURES 90)" >> config.cmake
+    echo "set(USE_NVSHMEM /opt/scratch/ycai/nvshmem/install/)" >> config.cmake
+    cmake ..
+    cmake --build . --parallel 90
 fi
-rm -r build
-mkdir build
-cd build
-cp ../cmake/config.cmake .
-echo "set(USE_LLVM ON)" >> config.cmake
-echo "set(USE_CUDA ON)" >> config.cmake
-cmake ..
-make -j24
+
+
+if [ $MLC_LLM -eq 1 ]; then
+    echo "build mlc-llm at $current_dir/mlc-llm/"
+    mkdir $current_dir/mlc-llm/build -p
+    cd $current_dir/mlc-llm/build
+    echo "set(TVM_SOURCE_DIR $current_dir/tvm)" > config.cmake
+    echo "set(USE_LLVM ON)" >> config.cmake
+    echo "set(USE_CUDA ON)" >> config.cmake
+    echo "set(USE_NVTX ON)" >> config.cmake
+    echo "set(USE_GRAPH_EXECUTOR ON)" >> config.cmake
+    echo "set(USE_GRAPH_EXECUTOR_CUDA_GRAPH ON)" >> config.cmake
+    echo "set(USE_VLLM ON)" >> config.cmake
+    echo "set(USE_THRUST ON)" >> config.cmake
+    echo "set(USE_CUTLASS ON)" >> config.cmake
+    echo "set(USE_CUBLAS ON)" >> config.cmake
+    echo "set(USE_NCCL ON)" >> config.cmake
+    echo "set(USE_FLASHINFER ON)" >> config.cmake
+    echo "set(CMAKE_CUDA_ARCHITECTURES 90)" >> config.cmake
+    echo "set(USE_NVSHMEM /opt/scratch/ycai/nvshmem/install/)" >> config.cmake
+    cmake ..
+    cmake --build . --parallel 90
+fi
--- load.sh
+++ load.sh
@@ -1,13 +1,15 @@
-if [ "$#" -eq 0 ] ; then
-    echo 'Need argument 1 for tvm directory name.'
-    exit 0
-fi
-export TVM_HOME="$PWD/$1" 
+export TVM_BACKTRACE=1
+
+export TVM_HOME="$PWD/tvm" 
 export PYTHONPATH="$TVM_HOME/python:$PYTHONPATH" 
-export TVM_LIBRARY_PATH="$TVM_HOME/build/" 
 export MYPYPATH="$TVM_HOME/python/:$MYPYPATH" 
+
+export MLC_HOME="$PWD/mlc-llm" 
+export PYTHONPATH="$MLC_HOME/python:$MLC_HOME:$PYTHONPATH" 
+export MYPYPATH="$MLC_HOME/python/:$MYPYPATH" 
+
+
 echo "TVM_HOME = $TVM_HOME" > .env
 echo "PYTHONPATH = $PYTHONPATH" >> .env
-echo "TVM_LIBRARY_PATH = $TVM_LIBRARY_PATH" >> .env
 echo "MYPYPATH = $MYPYPATH" >> .env
 echo "Done"
